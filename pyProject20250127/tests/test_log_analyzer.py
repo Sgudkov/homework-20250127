ea13820196574d7d630b7336fe4c374552947fcc
Sergey <sgudkovv@gmail.com>
2025-02-15 19:29:33 +0300

    Change tests

--- pyProject20250127/tests/test_log_analyzer.py
+++ pyProject20250127/tests/test_log_analyzer.py
@@ -1,3 +1,5 @@
+import os
+
 from log_analyzer import LogHandler
 
 
@@ -20,6 +22,7 @@ def test_log_analyzer_file_path():
         "LOG_DIR": "./log",
     }
     log = LogHandler(config, "")
+    assert os.path.isfile(config["LOG_DIR"])
     file_path, file_archive, last_date = log.get_latest_filepath()
     assert file_path is not None
     assert file_archive is not None
